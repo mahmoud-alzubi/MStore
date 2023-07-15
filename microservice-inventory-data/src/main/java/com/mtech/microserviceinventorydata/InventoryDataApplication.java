f662cb602d8fdc83081d4544961585066c9ba49c
Mahmoud AlZu'bi <m7moudzo3bi@gmail.com>
2023-07-15 04:08:39 +0300

    remove unused files.

--- microservice-inventory-data/src/main/java/com/mtech/microserviceinventorydata/InventoryDataApplication.java
+++ microservice-inventory-data/src/main/java/com/mtech/microserviceinventorydata/InventoryDataApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InventoryDataApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceInventoryDataApplication.class, args);
+		SpringApplication.run(InventoryDataApplication.class, args);
 	}
 
 }
