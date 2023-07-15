32dd8f25831cbb8ada687ca10f16815c3febb102
Mahmoud AlZu'bi <m7moudzo3bi@gmail.com>
2023-07-15 10:45:39 +0300

    checking inventory before placing the order.

--- microservice-orders-data/src/main/java/com/mtech/microserviceorderdata/service/OrderServiceImpl.java
+++ microservice-orders-data/src/main/java/com/mtech/microserviceorderdata/service/OrderServiceImpl.java
@@ -1,14 +1,17 @@
 package com.mtech.microserviceorderdata.service;
 
+import com.mtech.commons.dto.InventoryResponse;
 import com.mtech.microserviceorderdata.dto.OrderLineItemDto;
 import com.mtech.microserviceorderdata.dto.OrderRequest;
 import com.mtech.microserviceorderdata.entity.Order;
 import com.mtech.microserviceorderdata.entity.OrderLineItem;
+import com.mtech.microserviceorderdata.exception.ProductNotFoundException;
 import com.mtech.microserviceorderdata.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,10 +20,12 @@ import java.util.UUID;
 @Slf4j
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository repository;
+    private final WebClient client;
 
     @Autowired
-    public OrderServiceImpl(OrderRepository repository) {
+    public OrderServiceImpl(OrderRepository repository, WebClient client) {
         this.repository = repository;
+        this.client = client;
     }
 
 
@@ -36,6 +41,29 @@ public class OrderServiceImpl implements OrderService {
     public void placeOrder(OrderRequest orderRequest) {
         try {
             log.info("placeOrder({})", orderRequest);
+
+
+            List<String> listOfCodes = orderRequest.getOrderLineItemList().stream().map(orderLineItem -> {
+                return orderLineItem.getSkuCode();
+            }).toList();
+
+            InventoryResponse[] inventoryResponseArray = client.get()
+                    .uri("http://localhost:8082/api/inventories",
+                            uriBuilder -> uriBuilder.queryParam("skuCode", listOfCodes).build())
+                    .retrieve()
+                    .bodyToMono(InventoryResponse[].class)
+                    .block();
+
+            if (inventoryResponseArray.length == 0) {
+                throw new ProductNotFoundException("Product is not in stock currently, please try another time.");
+            }
+
+            for (InventoryResponse invItemResponse : inventoryResponseArray) {
+                if (!invItemResponse.isInStock()) {
+                    throw new ProductNotFoundException("Product is not in stock currently, please try another time.");
+                }
+            }
+
             List<OrderLineItem> orderLineItemList = orderRequest.getOrderLineItemList().stream().map(
                     item -> mapOrderLineItemsToDto(item)).toList();
 
