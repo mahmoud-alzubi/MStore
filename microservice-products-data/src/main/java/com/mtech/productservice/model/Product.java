9913556eebf192986cc97046917dc38e721db77c
Mahmoud AlZu'bi <m7moudzo3bi@gmail.com>
2023-07-16 01:26:26 +0300

    fix Document value.

--- microservice-products-data/src/main/java/com/mtech/productservice/model/Product.java
+++ microservice-products-data/src/main/java/com/mtech/productservice/model/Product.java
@@ -8,7 +8,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document(collation = "products")
+@Document(value = "products")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
