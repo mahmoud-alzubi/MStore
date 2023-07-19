bb46ecf53db0291ca373a204b72e6600a599e4cb
Mahmoud AlZu'bi <m7moudzo3bi@gmail.com>
2023-07-19 18:43:40 +0300

    secure gateway with Keycloak using OAuth 2.0

---
+++ api-gateway/src/main/java/com/mtech/apigateway/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.mtech.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    private ServerHttpSecurity.OAuth2ResourceServerSpec.JwtSpec jwt;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity
+                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        Customizer<ServerHttpSecurity.CsrfSpec> csrfCustomizer;
+        serverHttpSecurity.csrf(Customizer.withDefaults());
+
+        return serverHttpSecurity.build();
+    }
+
+}
